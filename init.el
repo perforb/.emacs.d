be07c75cb3afb54f58ee282aaeb17e4ae16896a5
perforb <dev.perfumed.garden@gmail.com>
2015-02-11 23:35:39 +0900

    Modified the theme

--- init.el
+++ init.el
@@ -17,7 +17,6 @@
 ;; ------------------------------------------------------------------------
 ;; @ package.el
 
-;; http://rubikitch.com/package-initialize/
 (package-initialize)
 (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
