e89d6609027339bf630c53dad65dbeea071fb262
perforb <dev.perfumed.garden@gmail.com>
2015-02-24 10:15:53 +0900

    Added markdown-mode

--- inits/10-init-elisp.el
+++ inits/10-init-elisp.el
@@ -573,6 +573,22 @@
 (add-hook 'nxml-mode-hook 'set-newline-and-indent)
 
 ;; ------------------------------------------------------------------------
+;; @markdown-mode
+
+;; (install-elisp "http://jblevins.org/projects/markdown-mode/markdown-mode.el")
+;; http://jblevins.org/projects/markdown-mode/
+(autoload 'markdown-mode "markdown-mode.el"
+  "Major mode for editing Markdown files" t)
+
+(add-to-list 'auto-mode-alist '("\\.txt$" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.mdwn$" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.mdt$" . markdown-mode))
+
+;; org-mode の表作成を自動で利用可能にする
+(add-hook 'markdown-mode-hook 'turn-on-orgtbl)
+
+;; ------------------------------------------------------------------------
 ;; @Auto Revert
 
 ;; http://yoshiori.github.com/blog/2013/01/31/file-update-emacs/
